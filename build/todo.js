89563df7b40c344d9d5a88892017138749e7f6b9
Chris Wu <me@chriswu.me>
2016-01-14 18:14:00 -0800

    restructure

---
+++ build/todo.js
@@ -0,0 +1,11 @@
+'use strict';
+
+TodoPage = React.createClass({
+  displayName: 'TodoPage'
+});
+
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello, world!'
+), document.getElementById('todo-app'));
